1599dca738293701112b4e1cf2c38ebb730f6da7
Rmcstt <anisio.nanato@hotmail.com>
2023-08-26 20:06:04 -0300

    postado

--- pomodoro.py
+++ pomodoro.py
@@ -3,14 +3,20 @@ import os
 import pygame
 import sys
 
+# passe o comando no terminal "python pomodoro.py 25 5" como exemplo, sendo o promeiro parametro o tempo de trabalho e o segundo o tempo de descanso
 
-# this program need a mp3 file attached in assets folder
+work_minutes = int(sys.argv[1])
+break_minutes = int(sys.argv[2])
+pomodoro_cycles = 4
+
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+
 def format_time(minutes, seconds):
     return "{:02d}:{:02d}".format(minutes, seconds)
+
 
 def play_sound(sound_file):
     pygame.mixer.init()
@@ -19,15 +25,17 @@ def play_sound(sound_file):
     while pygame.mixer.music.get_busy():
         pygame.time.Clock().tick(10)
 
+
 def pomodoro_timer(work_time, break_time, cycles):
     play_sound("assets/8d82b5_MK_vs_DCU_Round_One_Sound_Effect.mp3")
-    for cycle in range(1, cycles + 1):  # +1 to include the last cycle
+    for cycle in range(1, cycles + 1):
         for remaining_minutes in range(work_time, -1, -1):
             for remaining_seconds in range(59, -1, -1):
                 clear_screen()
                 print("Pomodoro Timer\n")
                 print(f"Round: {cycle}/{cycles}")
-                print(f"Work Timer: " + format_time(remaining_minutes, remaining_seconds))
+                print(f"Work Timer: " +
+                      format_time(remaining_minutes, remaining_seconds))
                 time.sleep(1)
 
         play_sound("assets/8d82b5_MK_vs_DCU_Finish_Him_Sound_Effect.mp3")
@@ -36,20 +44,18 @@ def pomodoro_timer(work_time, break_time, cycles):
             for remaining_seconds in range(59, -1, -1):
                 clear_screen()
                 print("Pomodoro Timer\n")
-                print(f"Round: {cycle}/{cycles}")
-                print("Break Timer: " + format_time(remaining_minutes, remaining_seconds))
+                print("")
+                print("Break Timer: " +
+                      format_time(remaining_minutes, remaining_seconds))
                 time.sleep(1)
-        if cycle  == 1:
+        if cycle == 1:
             play_sound("assets/8d82b5_MK_vs_DCU_Round_Two_Sound_Effect.mp3")
         elif cycle == 2:
-            play_sound("assets/8d82b5_Mortal_Kombat_3_Round_Three_Sound_Effect.mp3")
+            play_sound(
+                "assets/8d82b5_Mortal_Kombat_3_Round_Three_Sound_Effect.mp3")
         else:
             play_sound("assets/8d82b5_MK_vs_DCU_Final_Round_Sound_Effect.mp3")
 
 
 if __name__ == "__main__":
-    work_minutes = int(sys.argv[1])
-    break_minutes = int(sys.argv[2])
-    pomodoro_cycles = 4
-
     pomodoro_timer(work_minutes, break_minutes, pomodoro_cycles)
